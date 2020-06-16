aded63fedd1d2434773ee72ab9e7ebe7bd910ee7
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-06-16 13:35:34 +0700

    log data from api

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
         if(e.key === 'Enter') {
             const data = await fetchWeather(query);
 
+            console.log(data)
             setWeather(data);
             setQuery('');
         }
